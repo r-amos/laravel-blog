5363fb7cceb2ee6ae02055cbe40b68c82cc2d282
Rob Amos <rob@robamos.co.uk>
2018-09-13 22:44:04 +0100

    Adding Tag Model, Pivot Table, Factory & Controller

--- database/seeds/TagsTableSeeder.php
+++ database/seeds/TagsTableSeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
-class PostsTableSeeder extends Seeder
+class TagsTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -11,7 +11,6 @@ class PostsTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\Post::class, 50)
-            ->create();
+        factory(App\Tag::class, 10)->create();
     }
 }
