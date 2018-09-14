035da504bbb7bc6eaa5847b8bd89e9752b4bb890
Rob Amos <rob@robamos.co.uk>
2018-09-14 23:50:37 +0100

    Adding Custom Paginator With Filtering

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,18 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Pagination\Paginator;
 use App\Post;
 
 class PostsController extends Controller
 {
+    const PAGE_LIMIT = 5;   
     public function index()
-    {
-        return Post::simplePaginate(5);
+    {        
+       $posts = Post::latest()
+            ->filters(request(['month', 'year']))
+            ->get();
+        $posts = array_slice(
+            $posts->toArray(),
+            $this->getPaginationStart(Paginator::resolveCurrentPage()),
+            self::PAGE_LIMIT
+        );
+        return new Paginator($posts, self::PAGE_LIMIT);
     }
 
     public function show(Post $post)
     {
         return $post;
+    }
+
+    private function getPaginationStart($currentPage)
+    {
+        return (self::PAGE_LIMIT * $currentPage) - self::PAGE_LIMIT;
     }
 }
--- app/Post.php
+++ app/Post.php
@@ -3,11 +3,32 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Post extends Model
 {
     public function tags()
     {
         return $this->belongsToMany('App\Tag');
+    }
+    public function scopeFilters($query, $filters)
+    {   
+        if ($filters) {
+            if ($filters['month']) {
+                $query = $query
+                    ->whereMonth(
+                        'created_at', 
+                        Carbon::parse($filters['month'])->month
+                    );
+            }
+            if ($filters['year']) {
+                $query = $query
+                    ->whereYear(
+                        'created_at',
+                        $filters['year']
+                    );
+            }
+        }
+        return $query;
     }
 }
