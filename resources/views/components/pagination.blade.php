5d8ed200c76233c26c6de8b9616f330efcbab0e7
Rob Amos <rob@robamos.co.uk>
2018-09-15 02:19:18 +0100

    Adding Pagination Component

---
+++ resources/views/components/pagination.blade.php
@@ -0,0 +1,4 @@
+<h1>Pagination</h1>
+<a href={{ $paginator->previousPageUrl() }}>Previous Page</a>
+<a href={{ $paginator->nextPageUrl() }}>Next Page</a>
+
--- resources/views/pages/posts.blade.php
+++ resources/views/pages/posts.blade.php
@@ -2,11 +2,8 @@
 
 @section('content')
     <h1>Some Stuff</h1>
-
     @foreach($posts as $post)
     <h3>{{ $post['title'] }}</h3>
     @endforeach
-    {{ $posts->links('components.pagination')}}
-
-    {{$posts->links()}}
+    {{ $posts->links('components.pagination', ['paginator' => $posts]) }}
 @endsection
