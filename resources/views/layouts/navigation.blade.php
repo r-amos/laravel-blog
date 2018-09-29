d18b1aba7be25cb762cf0fd51c37a5ebf135b142
Rob Amos <rob@robamos.co.uk>
2018-09-29 22:19:04 +0100

    Remove Contact

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -2,6 +2,6 @@
     <ul class="navigation navigation__container container">
         <li class="navigation navigation__item nav-link"><a href="/">Ramblings</a></li>
         <li class="navigation navigation__item nav-link"><a href="/about">About</a></li>
-        <li class="navigation navigation__item nav-link"><a href="/contact">Contact</a></li>
+        <!--<li class="navigation navigation__item nav-link"><a href="/contact">Contact</a></li>-->
     </ul>
 </nav>
