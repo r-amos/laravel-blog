cc8baa96eaa48190c2d09d9afa5d20c4055e0654
Rob Amos <rob@robamos.co.uk>
2018-09-30 10:38:00 +0100

    Upating PostController Create doc block

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,31 +2,32 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Illuminate\Pagination\Paginator;
-
 use App\Post;
 use App\Tag;
+use Illuminate\Http\Request;
 
 /**
  * PostsController class responsible for handling requests
- * for Post Models. 
- * 
+ * for Post Models.
+ *
  * PostsController class contains the logic responsible for handling
  * create, read, update and delete requests in relation to a Post or
- * collection of Posts. 
+ * collection of Posts.
  */
 class PostsController extends Controller
 {
 
+    /**
+     * Define the number of posts per page to be used in pagination
+     */
     const PAGE_LIMIT = 5;
 
     /**
      * Constructs PostController instance
-     * 
+     *
      * Contstructor function is responsible for creating and returning a PostsController instance
      * along with assigning the authentication middleware to the 'create' and 'store' actions.
-     * 
+     *
      * @return void
      */
     public function __construct()
@@ -37,7 +38,7 @@ class PostsController extends Controller
 
     /**
      * Return Posts View with Pagination Posts Data
-     * 
+     *
      * Gets a collection of Posts, with Tags relationship populated, ordered by the latest
      * with an optional filter of the month and / or of interest applied. The collection
      * is converted to an array, and passed to Post::getPageItems, the result
@@ -46,7 +47,7 @@ class PostsController extends Controller
      * @return \Illuminate\View\View
      */
     public function index(): \Illuminate\View\View
-    {                  
+    {
         return view(
             'pages.posts',
             [
@@ -56,7 +57,7 @@ class PostsController extends Controller
                         ->filters(request(['month', 'year']))
                         ->get()
                         ->toArray()
-                )
+                ),
             ]
         );
     }
@@ -65,8 +66,8 @@ class PostsController extends Controller
      * Returns post view
      *
      * For a specific Post provided, calls Post->convertMarkdownContent
-     * and passes as data to the 'pages.post' view.
-     * 
+     * and passes the convertedc (makrdown to html) data to the 'pages.post' view.
+     *
      * @param Post $post
      * @return \Illuminate\View\View
      */
@@ -75,21 +76,39 @@ class PostsController extends Controller
         $post->convertMarkdownContent();
         return view('pages.post', compact('post'));
     }
-
+    /**
+     * Return Post Creation View
+     *
+     * Return the post creation view, along with an array of datat
+     * 'tags' which contains the tags available for associating to
+     * a created post.
+     *
+     * @return \Illuminate\View\View
+     */
     public function create(): \Illuminate\View\View
     {
         return view('pages.create-post', [
-            'tags' => \App\Tag::all()->pluck(['name'])
+            'tags' => \App\Tag::all()->pluck(['name']),
         ]);
     }
-
+    /**
+     * Creates A Post In The Database & Redirects To Posts Index
+     *
+     * Give a request containing post information (title, description, content) the
+     * currently authenticated user is retrieved and used to create a new Post Model.
+     * The tags relationship is also created by calling createTagsRelationship on the
+     * newly created blog model and passing in the topics recieved as part of the request.
+     *
+     * @param Request $request
+     * @return \Illuminate\Routing\Redirector|\Illuminate\Http\RedirectResponse
+     */
     public function store(Request $request)
-    {                
+    {
         (Post::create([
             'title' => $request['blog-title'],
             'description' => $request['blog-description'],
             'content' => $request['blog-content'],
-            'user_id' => auth()->id()
+            'user_id' => auth()->id(),
         ]))
             ->createTagsRelationship($request['blog-topic']);
         return redirect('/posts');
@@ -99,19 +118,19 @@ class PostsController extends Controller
     {
         return view('pages.edit-post', [
             'post' => $post,
-            'tags' => \App\Tag::all()->pluck(['name'])
+            'tags' => \App\Tag::all()->pluck(['name']),
         ]);
     }
 
     public function update(Post $post, Request $request)
-    {   
+    {
 
         // Update Post With Request Data.
         $post->title = $request['blog-title'];
         $post->description = $request['blog-description'];
         $post->content = $request['blog-content'];
         $post->save();
-        
+
         // Update Tags Relationship
         $post->updateTagsRelationship($request['blog-topic']);
 
