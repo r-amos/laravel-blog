7315610846a5453e798ab54decf11a2fe1040432
Rob Amos <rob@robamos.co.uk>
2018-10-28 11:00:07 +0000

    Adding Dynamic Canonical URLs

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -6,7 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Rob Amos | @yield('title', 'Web Developer')</title>
     <meta name="description" content="@yield('description','Personal site for Rob Amos, Web Developer based in the South West.')">
-     <!-- Styles -->
+    <link rel="canonical" href="{{ @URL::current() }}">
+    <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Asap:400,700" rel="stylesheet">
 </head>
